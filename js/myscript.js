46af2f798fede87bfc97b85f0f59cce5043c4b35
Gabrielecorti01 <gcorti.info@gmail.com>
2021-08-26 16:41:19 +0200

    STAMPA NOMI IN ORDINE ALFABETICO

---
+++ js/myscript.js
@@ -0,0 +1,13 @@
+
+// chiedi all’utente il cognome
+// inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+const cognomi = ["Rossi", "Neri", "Blu","Verde","Bianco"];
+
+
+var cognomeUtente = prompt("Inserisci il tuo cognome!");
+cognomi.push(cognomeUtente)
+
+console.log(cognomi.sort())
