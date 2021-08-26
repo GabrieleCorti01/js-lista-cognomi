097f862f8ff67fa9ed7461a04009b2a62788f5c4
Gabrielecorti01 <gcorti.info@gmail.com>
2021-08-26 18:28:24 +0200

    Non completo al 100%

--- js/myscript.js
+++ js/myscript.js
@@ -4,10 +4,25 @@
 // stampa la lista ordinata alfabeticamente
 // scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 
+// const cognomi = ["Rossi", "Neri", "Blu","Verde","Bianco"];
+// var cognomeUtente = prompt("Inserisci il tuo cognome!");
+// cognomi.push(cognomeUtente)
+// console.log(cognomi.sort())
+
 const cognomi = ["Rossi", "Neri", "Blu","Verde","Bianco"];
-
-
 var cognomeUtente = prompt("Inserisci il tuo cognome!");
-cognomi.push(cognomeUtente)
 
-console.log(cognomi.sort())
+for (let i = 0; i < 6; i++) {
+  let listaCognomi = console.log(cognomi [i]);
+  cognomi.push(cognomeUtente)
+
+  console.log(cognomi.sort() [i])
+}
+
+
+
+
+
+
+
+
